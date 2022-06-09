94a8b5df4c2dbd3ffaa0082fce86592e095c08a7
TsheringDorji99 <71002711+TsheringDorji99@users.noreply.github.com>
2022-06-09 23:09:58 +0600

    Add files via upload

---
+++ Mini_Project/app.js
@@ -0,0 +1,72 @@
+const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+const header = document.querySelector('.header.container');
+
+hamburger.addEventListener('click', () => {
+	hamburger.classList.toggle('active');
+	mobile_menu.classList.toggle('active');
+});
+
+document.addEventListener('scroll', () => {
+	var scroll_position = window.scrollY;
+	if (scroll_position > 250) {
+		header.style.backgroundColor = '#29323c';
+	} else {
+		header.style.backgroundColor = 'transparent';
+	}
+});
+
+// menu_item.forEach((item) => {
+// 	item.addEventListener('click', () => {
+// 		hamburger.classList.toggle('active');
+// 		mobile_menu.classList.toggle('active');
+// 	});
+// });
+// const wrapper = document.querySelector(".wrapper");
+// const fileName = document.querySelector(".file-name");
+// const defaultBtn = document.querySelector("#default-btn");
+// const customBtn = document.querySelector("#custom-btn");
+// const cancelBtn = document.querySelector("#cancel-btn i");
+// const img = document.querySelector("#img1");
+// let regExp = /[0-9a-zA-Z\^\&\'\@\{\}\[\]\,\$\=\!\-\#\(\)\.\%\+\~\_ ]+$/;
+// function defaultBtnActive(){
+//   defaultBtn.click();
+// }
+// defaultBtn.addEventListener("change", function(){
+//   const file = this.files[0];
+//   if(file){
+//     const reader = new FileReader();
+//     reader.onload = function(){
+//       const result = reader.result;
+//       img.src = result;
+//       wrapper.classList.add("active");
+//     }
+//     cancelBtn.addEventListener("click", function(){
+//       img.src = "";
+//       wrapper.classList.remove("active");
+//     })
+//     reader.readAsDataURL(file);
+//   }
+//   if(this.value){
+//     let valueStore = this.value.match(regExp);
+//     fileName.textContent = valueStore;
+//   }
+// });
+
+// {% comment %} flatpickr("input[type=datetime-local]", {}); {% endcomment %}
+
+// config = {
+//   dateFormat: "Y-m-d",
+//   minDate: "2020-01-15",
+//   maxDate: "2022-04-01"
+// }  
+// flatpickr("input[type=datetime-local]", config); 
+// {% comment %}  To pick time  {% endcomment %}
+// {% comment %} config = {
+//   enableTime: true,
+//   dateFormat: "Y-m-d H:i",
+//   altInput: true,
+//   altFormat: "F j, Y (h: S K)"
+// }  
+// flatpickr("input[type=datetime-local]", config); {% endcomment %}
